ec98984ded5ca09c0ba29fa7f1bf5a170d3525c8
hojiakbarandaqulov <hojiakbarandaqulov5@gmail.com>
2024-05-25 21:06:22 +0500

    email finish commit

--- src/main/java/dasturlash/uz/controller/AuthController.java
+++ src/main/java/dasturlash/uz/controller/AuthController.java
@@ -20,6 +20,7 @@ public class AuthController {
         String body = authService.registration(dto);
         return ResponseEntity.ok().body(body);
     }
+
     @PostMapping("/login")
     public ResponseEntity<AuthResponseDTO> login(@Valid @RequestBody LoginDTO dto) {
         AuthResponseDTO body = authService.login(dto);
@@ -31,9 +32,13 @@ public class AuthController {
         String body = authService.authorizationVerification(userId);
         return ResponseEntity.ok().body(body);
     }
+
     @GetMapping("/registration/resend/{email}")
     public ResponseEntity<String> registrationResend(@PathVariable("email") String email) {
         String body = authService.registrationResendEmail(email);
+        return ResponseEntity.ok().body(body);
+    }
+
     @GetMapping("/verification/{userId}")
     public ResponseEntity<String> resent(@PathVariable("userId") Integer userId) {
         String body = authService.authorizationVerification(userId);
